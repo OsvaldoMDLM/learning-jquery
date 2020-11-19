c3e8ba65192b5abae0859f3a5c6b568d929cc86c
Osvaldo Medina <62674782+OsvaldoMDLM@users.noreply.github.com>
2020-11-18 21:55:24 -0600

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('h1').addClass('titRio')
+    $('span').text('rios');
+    $('h2').css({"background-color":"#75f075" ,"text-align":"center"});
+    $('#rioUno').attr('src','https://futurorenovable.cl/wp-content/uploads/2015/08/rios-agua-img-noticias.masverdedigital-985x600.jpg');
+    $('#rioDos').attr('src', 'http://awsassets.wwf.es/img/z1c5564_michael_poliza_wwf_webres_81544.jpg');
+    $('body').append(`<p style='border-top: 1px solid; text-align: center;'>&copy; Desarrollado por: <a href="https://github.com/OsvaldoMDLM" target="_blank">Osvaldo Medina</a></p>`);
+})
